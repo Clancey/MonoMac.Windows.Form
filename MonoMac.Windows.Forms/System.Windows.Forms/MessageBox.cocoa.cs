5045e29ba0ab5b57c09693cfcb6d69609cf75c2d
James Clancey <james.clancey@gmail.com>
2011-02-10 23:20:23 -0900

    Message box works

--- MonoMac.Windows.Forms/System.Windows.Forms/MessageBox.cocoa.cs
+++ MonoMac.Windows.Forms/System.Windows.Forms/MessageBox.cocoa.cs
@@ -1,112 +1,266 @@
 using System;
+using System.Drawing;
 using MonoMac.AppKit;
 namespace System.Windows.Forms
 {
-	public partial class MessageBox : NSAlert
-	{
-		public static DialogResult Show (string Text)
-		{
-			var alert = new MessageBox ();
-			alert.MessageText = Text;
-			alert.RunModal ();
-			return alert.result;
-		}
-		public static DialogResult Show (object sender, string Text, string Title,MessageBoxButtons buttons,MessageBoxIcon icon)
-		{
-			var alert = new MessageBox ();
-			alert.MessageText = Title;
-			alert.InformativeText = Text;
-			alert.SetupButtons(buttons);
-			alert.SetupIcon(icon);
-			var result = GetResult(alert.RunModal (),buttons);
-			return result;
-		}
-		public static DialogResult GetResult(int result,MessageBoxButtons buttons)
-		{
-			switch(buttons)
-			{
-			case MessageBoxButtons.OK:
-				return DialogResult.OK;
-			case MessageBoxButtons.OKCancel :
-				if(result > 1000)
-					return DialogResult.OK;
-				return DialogResult.Cancel;
-			case MessageBoxButtons.AbortRetryIgnore:
-				if(result == 1000)
-					return DialogResult.Ignore;
-				else if (result == 1001)
-					return DialogResult.Retry;
-				return DialogResult.Abort;
-			case MessageBoxButtons.RetryCancel:
-				if(result == 1000)
-					return DialogResult.Cancel;
-				return DialogResult.Retry;
-			case MessageBoxButtons.YesNo:
-				if(result == 1000)
-					return DialogResult.No;
-				return DialogResult.Yes;
-			case MessageBoxButtons.YesNoCancel:
-				if(result == 1000)
-					return DialogResult.Cancel;
-				else if (result == 1001)
-					return DialogResult.No;
-				else return DialogResult.Yes;
-			default :
-				return DialogResult.Cancel;
-				
-			}
-		}
 		
-		public MessageBox ()
-		{
-		}
-		public void SetupIcon(MessageBoxIcon icon)
-		{
-			switch(icon)
-			{
-			case MessageBoxIcon.Error:
-				this.AlertStyle = NSAlertStyle.Critical;
-				break;
-			case MessageBoxIcon.Warning :
-				this.AlertStyle = NSAlertStyle.Warning;
-				break;
-			default :
-				this.AlertStyle = NSAlertStyle.Informational;
-				break;
-			}
-		}
-		public void SetupButtons(MessageBoxButtons buttons)
-		{
-			switch (buttons)
-			{
-			case MessageBoxButtons.OK:
-					return;
-			case MessageBoxButtons.OKCancel:
-				this.AddButton("Cancel");
-				this.AddButton("OK");
-				break;
-			case MessageBoxButtons.AbortRetryIgnore :
-				this.AddButton("Ignore");
-				this.AddButton("Retry");
-				this.AddButton("Abort");
-				break;
-			case MessageBoxButtons.RetryCancel:
-				this.AddButton("Cancel");
-				this.AddButton("Retry");
-				break;
-			case MessageBoxButtons.YesNo:
-				this.AddButton("No");
-				this.AddButton("Yes");
-				break;
-			case MessageBoxButtons.YesNoCancel:
-				this.AddButton("Cancel");
-				this.AddButton("No");
-				this.AddButton("Yes");
-				break;
-				
-			}
-		}
-		public DialogResult result = DialogResult.OK;
+	internal
+	enum AlertType {
+		Default		= 1,
+		Error		= 2,
+		Question	= 3,
+		Warning		= 4,
+		Information	= 5
 	}
+	
+		#region Private MessageBoxForm class
+		internal class MessageBoxForm : NSAlert
+		{
+			#region MessageBoxFrom Local Variables
+			const int space_border = 10;
+			const int button_width = 86;
+			const int button_height = 23;
+			const int button_space = 5;
+			const int space_image_text= 10;
+		
+			MessageBoxIcon Icon;
+			string Text;
+			string			msgbox_text;
+			bool			size_known	= false;
+			Icon			icon_image;
+			RectangleF		text_rect;
+			MessageBoxButtons	msgbox_buttons;
+			MessageBoxDefaultButton	msgbox_default;
+			bool			buttons_placed	= false;
+			int			button_left;
+			Button[]		buttons = new Button[4];
+			bool                    show_help;
+			string help_file_path;
+			string help_keyword;
+			HelpNavigator help_navigator;
+			object help_param;
+			AlertType		alert_type;
+			#endregion	// MessageBoxFrom Local Variables
+		
+			#region MessageBoxForm Constructors
+			public MessageBoxForm (IWin32Window owner, string text, string caption,
+					       MessageBoxButtons buttons, MessageBoxIcon icon,
+					       bool displayHelpButton)
+			{
+				show_help = displayHelpButton;
+			
+				Icon = icon;
+				switch (icon) {
+					case MessageBoxIcon.None: {
+						icon_image = null;
+						alert_type = AlertType.Default;
+						break;
+					}
+
+					case MessageBoxIcon.Error: {		// Same as MessageBoxIcon.Hand and MessageBoxIcon.Stop
+						icon_image = SystemIcons.Error;
+						alert_type = AlertType.Error;
+						break;
+					}
+
+					case MessageBoxIcon.Question: {
+ 						icon_image = SystemIcons.Question;
+						alert_type = AlertType.Question;
+						break;
+					}
+
+					case MessageBoxIcon.Asterisk: {		// Same as MessageBoxIcon.Information
+						icon_image = SystemIcons.Information;
+						alert_type = AlertType.Information;
+						break;
+					}
+
+					case MessageBoxIcon.Warning: {		// Same as MessageBoxIcon.Exclamation:
+						icon_image = SystemIcons.Warning;
+						alert_type = AlertType.Warning;
+						break;
+					}
+				}
+
+				msgbox_text = text;
+				msgbox_buttons = buttons;
+				msgbox_default = MessageBoxDefaultButton.Button1;
+
+				this.Text = caption;
+			}
+
+			public MessageBoxForm (IWin32Window owner, string text, string caption,
+					MessageBoxButtons buttons, MessageBoxIcon icon,
+					MessageBoxDefaultButton defaultButton, MessageBoxOptions options, bool displayHelpButton)
+				: this (owner, text, caption, buttons, icon, displayHelpButton)
+			{
+				msgbox_default = defaultButton;
+			}
+
+			public MessageBoxForm (IWin32Window owner, string text, string caption,
+					       MessageBoxButtons buttons, MessageBoxIcon icon)
+				: this (owner, text, caption, buttons, icon, false)
+			{
+			}
+			#endregion	// MessageBoxForm Constructors
+
+			#region MessageBoxForm Methods
+			public void SetHelpData (string file_path, string keyword, HelpNavigator navigator, object param)
+			{
+				help_file_path = file_path;
+				help_keyword = keyword;
+				help_navigator = navigator;
+				help_param = param;
+			}
+			
+			internal string HelpFilePath {
+				get { return help_file_path; }
+			}
+			
+			internal string HelpKeyword {
+				get { return help_keyword; }
+			}
+			
+			internal HelpNavigator HelpNavigator {
+				get { return help_navigator; }
+			}
+			
+			internal object HelpParam {
+				get { return help_param; }
+			}
+			
+			public DialogResult RunDialog ()
+			{
+				MessageText = Text;
+				InformativeText = msgbox_text;
+				SetupButtons(msgbox_buttons);
+				SetupIcon(Icon);
+				var result = GetResult(this.RunModal (),msgbox_buttons);
+				return result;
+			}
+			
+
+			#endregion	// MessageBoxForm Methods
+
+			#region Functions for Adding buttons
+			public static DialogResult GetResult(int result,MessageBoxButtons buttons)
+			{
+				switch(buttons)
+				{
+				case MessageBoxButtons.OK:
+					return DialogResult.OK;
+				case MessageBoxButtons.OKCancel :
+					if(result > 1000)
+						return DialogResult.OK;
+					return DialogResult.Cancel;
+				case MessageBoxButtons.AbortRetryIgnore:
+					if(result == 1000)
+						return DialogResult.Ignore;
+					else if (result == 1001)
+						return DialogResult.Retry;
+					return DialogResult.Abort;
+				case MessageBoxButtons.RetryCancel:
+					if(result == 1000)
+						return DialogResult.Cancel;
+					return DialogResult.Retry;
+				case MessageBoxButtons.YesNo:
+					if(result == 1000)
+						return DialogResult.No;
+					return DialogResult.Yes;
+				case MessageBoxButtons.YesNoCancel:
+					if(result == 1000)
+						return DialogResult.Cancel;
+					else if (result == 1001)
+						return DialogResult.No;
+					else return DialogResult.Yes;
+				default :
+					return DialogResult.Cancel;
+					
+				}
+			}
+			
+			public void SetupIcon(MessageBoxIcon icon)
+			{
+				switch(icon)
+				{
+				case MessageBoxIcon.Error:
+					this.AlertStyle = NSAlertStyle.Critical;
+					break;
+				case MessageBoxIcon.Warning :
+					this.AlertStyle = NSAlertStyle.Warning;
+					break;
+				default :
+					this.AlertStyle = NSAlertStyle.Informational;
+					break;
+				}
+			}
+			
+			public void SetupButtons(MessageBoxButtons buttons)
+			{
+				switch (buttons)
+				{
+				case MessageBoxButtons.OK:
+						return;
+				case MessageBoxButtons.OKCancel:
+					this.AddButton("Cancel");
+					this.AddButton("OK");
+					break;
+				case MessageBoxButtons.AbortRetryIgnore :
+					this.AddButton("Ignore");
+					this.AddButton("Retry");
+					this.AddButton("Abort");
+					break;
+				case MessageBoxButtons.RetryCancel:
+					this.AddButton("Cancel");
+					this.AddButton("Retry");
+					break;
+				case MessageBoxButtons.YesNo:
+					this.AddButton("No");
+					this.AddButton("Yes");
+					break;
+				case MessageBoxButtons.YesNoCancel:
+					this.AddButton("Cancel");
+					this.AddButton("No");
+					this.AddButton("Yes");
+					break;
+					
+				}
+			}
+			
+			#endregion
+
+
+#if NET_2_0
+		
+			#region UIA Framework: Methods, Properties and Events
+
+			internal string UIAMessage {
+				get { return msgbox_text; }
+			}
+
+			internal Rectangle UIAMessageRectangle {
+				get { 
+					return new Rectangle ((int) text_rect.X,
+					                      (int) text_rect.Y, 
+					                      (int) text_rect.Width, 
+					                      (int) text_rect.Height); 
+				}
+			}
+
+			internal Rectangle UIAIconRectangle {
+				get { 
+					return new Rectangle (space_border, 
+					                      space_border, 
+							      icon_image == null ? -1 : icon_image.Width, 
+							      icon_image == null ? -1 : icon_image.Height);
+				}
+			}
+
+			#endregion
+
+#endif
+		}
+		#endregion	// Private MessageBoxForm class
+	
 }
 
--- TestLoad/TestWinform.cs
+++ TestLoad/TestWinform.cs
@@ -8,6 +8,7 @@ class MyForm : Form
 {
 
 	private System.Windows.Forms.Button button1;
+	private System.Windows.Forms.Button button2;
 	private System.Windows.Forms.TextBox textBox1;
 	private System.Windows.Forms.TextBox textBox2;
 	private System.Windows.Forms.CheckBox checkBox1;
@@ -19,6 +20,7 @@ class MyForm : Form
 	{
 		this.listbox1 = new System.Windows.Forms.ListBox();
 		this.button1 = new System.Windows.Forms.Button ();
+		this.button2 = new System.Windows.Forms.Button ();
 		this.textBox1 = new System.Windows.Forms.TextBox ();
 		this.textBox2 = new System.Windows.Forms.TextBox ();
 		this.checkBox1 = new System.Windows.Forms.CheckBox ();
@@ -53,9 +55,21 @@ class MyForm : Form
 		this.button1.Name = "button1";
 		this.button1.Size = new System.Drawing.Size (75, 25);
 		this.button1.TabIndex = 0;
-		this.button1.Text = "button1";
+		this.button1.Text = "Show Modal";
 		//this.button1.UseVisualStyleBackColor = true;
 		this.button1.Click += new System.EventHandler (this.button1_Click);
+		// 
+		// button2
+		// 
+		this.button2.Location = new System.Drawing.Point (150, 76);
+		this.button2.Name = "button1";
+		this.button2.Size = new System.Drawing.Size (150, 25);
+		this.button2.TabIndex = 0;
+		this.button2.Text = "Show Message box";
+		//this.button1.UseVisualStyleBackColor = true;
+		this.button2.Click += delegate {
+			MessageBox.Show("Text","Caption");
+		};
 		// 
 		// textBox1
 		// 
@@ -114,6 +128,7 @@ class MyForm : Form
 		this.Controls.Add (this.textBox2);
 		this.Controls.Add (this.textBox1);
 		this.Controls.Add (this.button1);
+		this.Controls.Add (this.button2);
 		this.Controls.Add(this.trackBar1);
 		this.Controls.Add(this.label1);
 		this.Name = "Form1";
